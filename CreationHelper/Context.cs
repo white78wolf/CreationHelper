4a003b64fdf849a9939b4cb79332efaa20b9cdda
Андрей Цепков <cepkov78@mail.ru>
2025-10-09 11:02:00 +0600

    create Context class

---
+++ CreationHelper/Context.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreationHelper
+{
+    internal class Context
+    {
+        private string _context;
+        public Context(string context )
+        {
+            _context = context;
+        }
+    }
+}
